a1d245837afbb63c46e3d55123b362ee2baeb6b7
Paul Barthmaier <paul@barthmaier.rocks>
2018-12-31 11:18:06 -0500

    Merge remote-tracking branch 'upstream/master'

--- pmpro-add-member-admin.php
+++ pmpro-add-member-admin.php
@@ -1,26 +1,25 @@
 <?php
-/**
- * Plugin Name: Paid Memberships Pro - Add Member Admin
- * Plugin URI: https://www.paidmembershipspro.com/add-ons/add-member-admin-add-on/
- * Description: Allow admins to add members in the WP dashboard.
- * Version: .3.1
- * Author: Stranger Studios
- * Author URI: https://www.strangerstudios.com
- * Text Domain: pmpro-add-member-admin
- * Domain Path: /languages
- */
+/*
+Plugin Name: Paid Memberships Pro - Add Member From Admin
+Plugin URI: https://www.paidmembershipspro.com/add-ons/add-member-admin-add-on/
+Description: Allow admins to add members in the WP dashboard.
+Version: .4
+Author: Stranger Studios
+Author URI: https://www.paidmembershipspro.com
+Text Domain: pmpro-add-member-admin
+Domain Path: /languages
+*/
 
+/*
+	* Add "Add Member" link under Memberships.
+	* Form with fields, username, email (find user), password (random), name, level, expiration (auto), credit card
+	* Add user, apply level, save order
+	* Filters for adding additional fields.
+*/
 
-/**
- * Add "Add Member" link under Memberships.
- * Form with fields, username, email (find user), password (random), name, level, expiration (auto), credit card
- * Add user, apply level, save order
- * Filters for adding additional fields.
- */
-
-/**
- * Add Menu Item
- */
+/*
+	Add Menu Item
+*/
 function pmproama_pmpro_add_pages() {
 	$cap = apply_filters( 'pmpro_add_member_cap', 'edit_users' );
 
@@ -32,9 +31,9 @@ function pmpro_addmember() {
 	require_once( dirname( __FILE__ ) . '/adminpages/addmember.php' );
 }
 
-/**
- * Admin Bar
- */
+/*
+	Admin Bar
+*/
 function pmproama_admin_bar_menu() {
 	global $wp_admin_bar;
 
@@ -58,8 +57,39 @@ function pmproama_admin_bar_menu() {
 }
 add_action( 'admin_bar_menu', 'pmproama_admin_bar_menu', 1001 );
 
+/* Register activation hook. */
+register_activation_hook( __FILE__, 'pmproama_admin_notice_activation_hook' );
+/**
+ * Runs only when the plugin is activated.
+ *
+ * @since 0.1.0
+ */
+function pmproama_admin_notice_activation_hook() {
+	// Create transient data.
+	set_transient( 'pmproama-admin-notice', true, 5 );
+}
+/**
+ * Admin Notice on Activation.
+ *
+ * @since 0.1.0
+ */
+function pmproama_admin_notice() {
+	// Check transient, if available display notice.
+	if ( get_transient( 'pmproama-admin-notice' ) ) { ?>
+		<div class="updated notice is-dismissible">
+			<p><?php printf( __( 'Thank you for activating. <a href="%s">Visit the Add Member admin page</a> to add new members.', 'pmpro-add-member-admin' ), get_admin_url( null, 'admin.php?page=pmpro-addmember' ) ); ?></p>
+		</div>
+		<?php
+		// Delete transient, only display this notice once.
+		delete_transient( 'pmproama-admin-notice' );
+	}
+}
+add_action( 'admin_notices', 'pmproama_admin_notice' );
+
 /**
  * Function to add links to the plugin action links
+ *
+ * @param array $links Array of links to be shown in plugin action links.
  */
 function pmproama_add_action_links( $links ) {
 	$cap = apply_filters( 'pmpro_add_member_cap', 'edit_users' );
@@ -73,7 +103,10 @@ function pmproama_add_action_links( $links ) {
 add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'pmproama_add_action_links' );
 
 /**
- *  Function to add links to the plugin row meta
+ * Function to add links to the plugin row meta
+ *
+ * @param array  $links Array of links to be shown in plugin meta.
+ * @param string $file Filename of the plugin meta is being shown for.
  */
 function pmproama_plugin_row_meta( $links, $file ) {
 	if ( strpos( $file, 'pmpro-add-member-admin.php' ) !== false ) {
@@ -86,9 +119,9 @@ function pmproama_plugin_row_meta( $links, $file ) {
 }
 add_filter( 'plugin_row_meta', 'pmproama_plugin_row_meta', 10, 2 );
 
-/**
- * Add action links
- */
+/*
+	Add action links
+*/
 function pmproama_action_links( $actions, $user ) {
 	$cap = apply_filters( 'pmpro_add_member_cap', 'edit_users' );
 
